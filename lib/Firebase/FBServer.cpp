c3a29fefa439b55eedf2be70777475140349f55b
siimtishler <siim.tishlr@gmail.com>
2023-12-21 17:00:35 +0200

    FIREBASE EMAIL VAL

--- lib/Firebase/FBServer.cpp
+++ lib/Firebase/FBServer.cpp
@@ -18,7 +18,7 @@
 #define API_KEY 						"API_KEY"
 #define DATABASE_URL 					"nutikapp-default-rtdb.europe-west1.firebasedatabase.app"
 
-#define FIREBASE_EMAIL 					"FIREBASE_EMAIL"
+#define FIREBASE_EMAIL 					"FIREBASE_EMAIL_VAL"
 #define FIREBASE_PASSWORD				"FIREBASE_PASSWORD4"
 
 #define UPDATE_DOOR_TIMEOUT_MS			SEC_TO_MS(1)
